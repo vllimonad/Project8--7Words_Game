88f8c97108ae2fe0f20af8ead18d90c137388119
Vlad Klunduk <klundukvlad40@gmail.com>
2023-08-16 15:59:26 +0200

    Initial Commit

---
+++ project8/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project8
+//
+//  Created by Vlad Klunduk on 16/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
